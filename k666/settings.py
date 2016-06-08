ecfdfbb27b56f0030f60620d418cfb6d5bc067c2
Orion Blastar <orionblastar@gmail.com>
2016-06-07 21:56:59 -0500

    Found the problem in the settings.py file and I think I fixed it

--- k666/settings.py
+++ k666/settings.py
@@ -111,11 +111,11 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/index.html'
 
 AUTHENTICATION_BACKENDS = (
-    ...
+    
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
-    ...
+    
 )
