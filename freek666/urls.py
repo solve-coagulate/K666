3998c4373eb33835789a789a7549227f899275a5
procrasti <procrasti@k5-stats.org>
2016-06-11 09:49:36 +0100

    Show a userlist!

--- freek666/urls.py
+++ freek666/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     # url(r'^index.html$', TemplateView.as_view(template_name="index.html")),
     url(r'^index.html$', RedirectView.as_view(url="/")),
     url(r'^$', IndexView.as_view(template_name="index.html")),
+    url(r'^u/$', UserListView.as_view()),
     # url(r'^$', RedirectView.as_view(url="/index.html")),
 ]
--- freek666/views.py
+++ freek666/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+
+from django.contrib.auth.models import User
 
 class IndexView(TemplateView):
     pass
+
+class UserListView(ListView):
+    model = User
+    template_name = "freek666/userlist.html"
