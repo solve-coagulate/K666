c5d6ae143f85c470c5fe9a9c3eb05f4c760f8f26
procrasti <procrasti@k5-stats.org>
2016-06-11 09:30:36 +0100

    Use our index view, also make / our root page, redirect in other direction.

--- freek666/urls.py
+++ freek666/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import include, url
 
 from django.views.generic.base import RedirectView
-from django.views.generic.base import TemplateView
+# from django.views.generic.base import TemplateView
+
+from .views import *
 
 urlpatterns = [
-    url(r'^index.html$', TemplateView.as_view(template_name="index.html")),
-    url(r'^$', RedirectView.as_view(url="/index.html")),
+    # url(r'^index.html$', TemplateView.as_view(template_name="index.html")),
+    url(r'^index.html$', RedirectView.as_view(url="/")),
+    url(r'^$', IndexView.as_view(template_name="index.html")),
+    # url(r'^$', RedirectView.as_view(url="/index.html")),
 ]
