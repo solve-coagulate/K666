6f7151733fc07531042056b81ee3c59eb5cc94d8
Orion Blastar <orionblastar@gmail.com>
2016-06-14 15:15:44 -0500

    fixing master due to messages not working with Django, rollback to before changes got made.

--- k666/settings.py
+++ k666/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'django_messages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,8 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+                ],
         },
     },
 ]
