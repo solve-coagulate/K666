2f0f0c7703894f38133a463d6e43541eb67a567f
Orion Blastar <orionblastar@gmail.com>
2016-06-18 10:22:37 -0500

    Fixing messages

--- k666/settings.py
+++ k666/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'django_messages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- k666/urls.py
+++ k666/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('freek666.urls')),
     url(r'^accounts/', include('allauth.urls')),
+    url (r'^messages/', include('django_messages.urls')),
 ]
