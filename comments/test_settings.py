6943d12c8cc38e4d8ccff280126cc0cfba2db897
solve-coagulate <ben.burns@gmail.com>
2025-06-06 19:11:32 +0800

    Add CSRF protection and validation to comment AJAX views

--- comments/test_settings.py
+++ comments/test_settings.py
@@ -2,8 +2,22 @@ SECRET_KEY='test'
 INSTALLED_APPS=[
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'freek666',
     'comments',
 ]
+MIDDLEWARE=[
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+ROOT_URLCONF='comments.urls'
+TEMPLATES=[{'BACKEND':'django.template.backends.django.DjangoTemplates','APP_DIRS':True,'OPTIONS':{'context_processors':['django.template.context_processors.request','django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages']}}]
 DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': ':memory:'}}
 USE_TZ=True
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
